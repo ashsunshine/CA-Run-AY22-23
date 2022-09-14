ea185978c84e0148e37081e8e577199567113cca
eltontay123 <65107920+eltontay123@users.noreply.github.com>
2022-09-15 00:01:24 +0800

    Add files via upload

---
+++ IS111 Code Clinic 1/Exercise 1/q3/q3.py
@@ -0,0 +1,40 @@
+##
+# Author : Stephen Pang
+# Created : July 31, 2021
+# Last Updated : July 31, 2021
+#
+# This question was adapted from the FizzBuzz programming question
+##
+
+def fizzbuzz(num):
+    # YOUR CODE GOES HERE
+
+    return None
+
+
+# DO NOT MODIFY THE CODE BELOW
+print('----Test Case 1----')
+result = fizzbuzz(4)
+print("str_formation(4)")
+print("Expected: 4" )
+print("Actual:   " + str(result))
+print()
+print('----Test Case 2----')
+result = fizzbuzz(25)
+print("str_formation(25)")
+print("Expected: Buzz" )
+print("Actual:   " + str(result))
+print()
+print('----Test Case 3----')
+result = fizzbuzz(15)
+print("str_formation(15)")
+print("Expected: FizzBuzz" )
+print("Actual:   " + str(result))
+print()
+print('----Test Case 4----')
+result = fizzbuzz(21)
+print("str_formation(21)")
+print("Expected: Fizz" )
+print("Actual:   " + str(result))
+print()
+
---
+++ IS111 Code Clinic 1/Hands On Exercise/q4/q4b.py
@@ -0,0 +1,38 @@
+##
+# Author : Stephen Pang
+# Created : August 19, 2021
+# Last Updated : August 19, 2021
+#
+# Copyright (C) 2021 Stephen Pang
+##
+
+def decode_formula(string):
+    # YOUR CODE GOES HERE
+
+    return None
+        
+        
+    
+        
+
+# DO NOT MODIFY THE CODE BELOW:
+print('----Test Case 1----')
+result = decode_formula('a3S-3o5AkE2b05#6')
+print("Expected: -352053" )
+print("Actual:   " + str(result))
+print()
+print('----Test Case 2----')
+result = decode_formula('@ka55Nsa/Sh/3iRo')
+print("Expected: 18" )
+print("Actual:   " + str(result))
+print()
+print('----Test Case 3----')
+result = decode_formula('2501024535-92')
+print("Expected: 2501024627" )
+print("Actual:   " + str(result))
+print()
+print('----Test Case 4----')
+result = decode_formula('SoCh0a5*sok2sapsms')
+print("Expected: 10" )
+print("Actual:   " + str(result))
+print()
